28fe82e45ebcc23d1ff59de3ef8c1a8c7638396e
Peter Schrimpel <peter.schrimpel10@gmail.com>
2026-01-03 00:18:32 +0100

    feat: public key validation

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/Peto10/SSH-like-Certificate-Authority-Service
 
 go 1.25.5
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+)
--- internal/api/api.go
+++ internal/api/api.go
@@ -1,10 +1,12 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 	"log/slog"
 	"strings"
 	"fmt"
+	"golang.org/x/crypto/ssh"
 )
 
 type Controller struct {
@@ -12,23 +14,60 @@ type Controller struct {
 	ValidTokens map[string]struct{}
 }
 
+type requestBody struct {
+	PublicKey string `json:"public_key"`
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+type signResponse struct {
+	Message    string `json:"message,omitempty"`
+	SignedCert string `json:"signed_cert,omitempty"`
+}
+
 func NewController(logger *slog.Logger, validTokens map[string]struct{}) *Controller {
 	return &Controller{Log: logger, ValidTokens: validTokens}
 }
 
 func (c *Controller) Sign(w http.ResponseWriter, r *http.Request) {
-	// TODO parse Authorisation header
-	c.Log.Info("Sign endpoint called")
+	w.Header().Set("Content-Type", "application/json")
+
 	authToken := r.Header.Get("Authorization")
 	fmt.Println("Authorization token received:", authToken)
 	isValid := c.validateAuthToken(authToken)
 	if !isValid{
-		fmt.Println("Unauthorized access attempt to Sign endpoint")
 		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(errorResponse{Error: "access token not valid"})
 		return
 	}
-	fmt.Println("Sign endpoint not yet implemented")
+
+	var reqBody requestBody
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorResponse{Error: "failed to decode request body"})
+		return
+	}
+
+	pubKey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(reqBody.PublicKey))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorResponse{Error: "failed to parse public key"})
+		return
+	}
+
+	if (pubKey.Type() != ssh.KeyAlgoED25519) {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorResponse{Error: "only ed25519 keys are supported"})
+		return
+	}
+
+	fmt.Println("Sign endpoint not yet implemented")	
+	// TODO: implement signing logic
 	w.WriteHeader(http.StatusNotImplemented)
+	json.NewEncoder(w).Encode(signResponse{Message: "Sign endpoint not yet implemented"})
 }
 
 func (c *Controller) validateAuthToken(token string) bool {
